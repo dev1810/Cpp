7968e7918699a7e3aebca2ec07577c3b523a2726
dev1810 <tshar113456@gmail.com>
2025-02-14 22:32:31 +0530

    lovebabbarsolution

---
+++ arrayquestionlb/swapalternate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int n = 5;
+    int arr[n]={1,2,3,4,5};
+
+   
+    for(int i =0;i<n;i+=2){
+        if(i+1 < n){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    for(int i = 0;i<n;i++){
+        cout<<arr[i];
+    }
+}
---
+++ arrayquestionlb/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+ for(int i =0;i<n;i+=2){
+        for(int j =1;j<n;j+=2){
+            
+            swap(arr[i],arr[j]);
+           //cout<<i;
+        }
+
+    }
